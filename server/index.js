a3db22910618c4d9e649d65dc3bb5666361a1eaf
root <root@Unaislaptop>
2023-12-26 16:25:45 +0530

    created Join component and chat component

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const socketio = require('socket.io')
 const http = require('http')
+const router = require('./router')
 
 const PORT = process.env.PORT || 5000
 
 const app = express();
 
+app.use(router)
+
 const server = http.createServer(app)
 const io = socketio(server);
+
+io.on('connection', (socket) =>{
+    console.log('We have new connection');
+
+    socket.on('disconnect', ()=>{
+        console.log('User has disconnected.');
+    })
+})
 
 server.listen(PORT, () =>{
     console.log(`Server has started on port ${PORT}`);
