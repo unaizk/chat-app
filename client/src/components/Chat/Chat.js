ff1d66f0e5c0df2fdbf09a0c349c37b119436ce9
root <root@Unaislaptop>
2023-12-26 17:10:24 +0530

    made the connection from client to backend

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Chat.css'
+import queryString from 'query-string';
+import io from 'socket.io-client'
+import { useLocation } from 'react-router-dom'; // Import useLocation
 
+
+let socket;
 const Chat = () => {
+
+  const [name , setName] = useState('');
+  const [room, setRoom] =useState('');
+  const ENDPOINT = 'localhost:5000'
+
+  const location = useLocation(); // Use useLocation hook to get the location object
+  useEffect(()=>{
+    const {room, name} = queryString.parse(location.search)
+
+    socket = io(ENDPOINT)
+    setName(name);
+    setRoom(room)
+  },[ENDPOINT, location.search])
   return (
     <div>Chat</div>
   )
--- server/index.js
+++ server/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
-const socketio = require('socket.io')
-const http = require('http')
-const router = require('./router')
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+const router = require('./router');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+      origin: "*", // Replace with your actual frontend origin
+    }
+  });
 
-app.use(router)
-
-const server = http.createServer(app)
-const io = socketio(server);
-
-io.on('connection', (socket) =>{
-    console.log('We have new connection');
-
-    socket.on('disconnect', ()=>{
-        console.log('User has disconnected.');
-    })
-})
-
-server.listen(PORT, () =>{
-    console.log(`Server has started on port ${PORT}`);
-})
+app.use(router);
+app.use(cors()); // Enable CORS for all routes
 
 
+io.on('connection', (socket) => {
+  console.log('We have a new connection');
+
+  socket.on('disconnect', () => {
+    console.log('User has disconnected.');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
